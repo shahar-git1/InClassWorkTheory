8e7b81dacd49f66200284136873f6cf8833e477b
Shahar <Shahar@DESKTOP-PVRVN70>
2026-02-04 08:21:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassWorkTheory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "First", "Second", "Third", "Fourth", "Fifth" };
+            Console.WriteLine(string.Join(Environment.MachineName, arr));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
